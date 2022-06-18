d6718563ae8508191e9ecfaaa6b22a7fc2916921
Zachary Reynolds <zreynolds@dminc.com>
2022-06-18 15:12:32 -0400

    Add initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './components/app.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-    imports: [FormsModule, BrowserModule],
+    imports: [FormsModule, BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
