dd0c8e73c0d3fc601e9cd21a607a8f1908871cd6
Zachary Reynolds <zreynolds@dminc.com>
2022-06-17 21:42:02 -0400

    Complete second step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
