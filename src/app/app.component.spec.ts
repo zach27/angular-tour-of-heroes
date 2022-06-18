feb02d252fb437e640c1cb8c7d17afc00f5a794c
Zachary Reynolds <zreynolds@dminc.com>
2022-06-18 12:06:16 -0400

    Add unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 describe('AppComponent', () => {
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [AppComponent],
+            declarations: [AppComponent, HeroesComponent],
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
 
     it(`should have as title 'angular-tour-of-heroes'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('angular-tour-of-heroes');
+        expect(app.title).toEqual('Tour of Heroes');
     });
 
     it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'angular-tour-of-heroes app is running!'
-        );
+        expect(compiled.querySelector('h1')?.textContent).toBe('Tour of Heroes');
     });
 });
--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 
-describe('HeroesComponent', () => {
+fdescribe('HeroesComponent', () => {
     let component: HeroesComponent;
     let fixture: ComponentFixture<HeroesComponent>;
 
+    const mockHeros: Hero[] = [
+        {
+            id: 1,
+            name: 'Fast Fredy',
+        },
+        {
+            id: 2,
+            name: 'Bold Bob',
+        },
+    ];
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [HeroesComponent, HeroDetailComponent],
+            providers: [HeroesComponent],
         }).compileComponents();
 
         fixture = TestBed.createComponent(HeroesComponent);
@@ -16,7 +30,69 @@ describe('HeroesComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should set selected hero when nothing is selected yet', () => {
+        const hero: Hero = {
+            id: 5,
+            name: 'Fred',
+        };
+
+        component.onSelect(hero);
+
+        expect(component.selectedHero).toEqual(hero);
+    });
+
+    it('should set selected hero when hero is changed', () => {
+        const firstHero: Hero = {
+            id: 5,
+            name: 'Fred',
+        };
+
+        const secondHero: Hero = {
+            id: 6,
+            name: 'Jane',
+        };
+
+        component.onSelect(firstHero);
+        component.onSelect(secondHero);
+
+        expect(component.selectedHero).toEqual(secondHero);
+    });
+
+    it('should clear selected hero when reselecting the same hero', () => {
+        const hero: Hero = {
+            id: 5,
+            name: 'Fred',
+        };
+
+        component.onSelect(hero);
+        component.onSelect(hero);
+
+        expect(component.selectedHero).toBeUndefined();
+    });
+
+    it('should render list of heros', () => {
+        component.heroes = mockHeros;
+        fixture.detectChanges();
+
+        const herosElement: HTMLElement = fixture.nativeElement;
+        const heroButtons = herosElement.querySelectorAll('.heroes button');
+        expect(heroButtons.length).toBe(2);
+    });
+
+    it('should show clicked hero', () => {
+        component.heroes = mockHeros;
+        fixture.detectChanges();
+
+        const htmlElement: HTMLElement = fixture.nativeElement;
+        const firstHero = htmlElement.querySelector('.heroes button') as HTMLElement;
+        firstHero.click();
+
+        fixture.detectChanges();
+
+        const detailComponent: HeroDetailComponent = fixture.debugElement.query(
+            By.directive(HeroDetailComponent)
+        ).componentInstance;
+
+        expect(detailComponent.hero?.id).toBe(1);
     });
 });
