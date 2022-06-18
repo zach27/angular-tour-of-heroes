30fc62f1ad9a95be4a9042abc99c9cbe991bc558
Zachary Reynolds <zreynolds@dminc.com>
2022-06-18 11:12:57 -0400

    Move hero detail feature to child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
     imports: [FormsModule, BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
