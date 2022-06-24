7855123e512425a162332420ae3a7821e7c8a7c5
Zachary Reynolds <zreynolds@dminc.com>
2022-06-24 19:14:54 -0400

    Add dashboard service tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { Hero } from 'src/app/hero';
+import { HeroService } from 'src/app/services/hero.service';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+    let component: DashboardComponent;
+    let fixture: ComponentFixture<DashboardComponent>;
+    let heroServiceMock: jasmine.SpyObj<HeroService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+    const mockHeroes: Hero[] = [
+        {
+            id: 1,
+            name: 'Fast Fredy',
+        },
+        {
+            id: 2,
+            name: 'Bold Bob',
+        },
+        {
+            id: 3,
+            name: 'Energetic Eddy',
+        },
+    ];
 
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        heroServiceMock = jasmine.createSpyObj<HeroService>('HeroService', ['getHeroes']);
+        heroServiceMock.getHeroes.and.returnValue(of(mockHeroes));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        await TestBed.configureTestingModule({
+            declarations: [DashboardComponent],
+            providers: [
+                {
+                    provide: HeroService,
+                    useValue: heroServiceMock,
+                },
+            ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(DashboardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should initialize with top heroes', () => {
+        const topHeroes: Hero[] = [
+            {
+                id: 2,
+                name: 'Bold Bob',
+            },
+            {
+                id: 3,
+                name: 'Energetic Eddy',
+            },
+        ];
+        expect(component.heroes).toEqual(topHeroes);
+    });
 });
